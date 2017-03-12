b92e9bb506041536b39ba9ec170a5b22a117f21f
DoctorEaker <kuiser1992@gmail.com>
2017-03-11 22:16:37 -0500

    working!

---
+++ CCPL/powers.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+void pew(long n) {
+  long potato = sqrt(n);
+  for (long i = 2; i <= potato; ++i) {
+    double pew = log10(n)/log10(i);
+    if (abs(pew - round(pew)) <= 0.0000000001) {
+      cout << i << "^" << pew << endl;
+      return;
+    }
+  }
+  cout << n << "^1" << endl;
+}
+
+int main() {
+  ios::sync_with_stdio(false);
+  long n;
+  while (cin >> n && n != -1) {
+    pew(n);
+  }
+  
+}
